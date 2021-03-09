54e3719b905ce4f6813001a1d34a5762bf1bba46
Shubham Nalawade <72625065+Shubzedm007@users.noreply.github.com>
2021-03-09 10:21:03 +0530

    Update C++ code to convert marks to CGPA and Percentage.cpp

--- C++ code to convert marks to CGPA and Percentage.cpp
+++ C++ code to convert marks to CGPA and Percentage.cpp
@@ -1,36 +1,25 @@
-
 #include<iostream>
 using namespace std;
-
 double CgpaCalc(double marks[], int n)
-{
-
+{      
 	double grade[n];
-
 	double cgpa, sum = 0;
-
-	
 	for(int i = 0; i < n; i++) 
 	{
 	grade[i] = (marks[i] / 10);
 	}
-
-
 	for(int i = 0; i < n; i++) 
 	{
 	sum += grade[i];
 	}
 	cgpa = sum / n;
-
 	return cgpa;
 }
-
 int main()
 {
 	int n = 5;
 	double marks [] = {90, 85, 88, 77, 92};
 	double cgpa = CgpaCalc(marks, n);
-		
 	cout << "CGPA = ";
 	printf("%.1f\n", cgpa);
 	cout << "CGPA Percentage = ";
